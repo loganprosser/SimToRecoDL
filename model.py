1e3fb79953ed9e4c7dc9e8b1aee26a6945227e92
loganprosser <loganpros@gmail.com>
2026-03-30 14:25:20 -0400

    fixing errors

--- model.py
+++ model.py
@@ -11,7 +11,7 @@ class SimpleTrackNet(nn.Module):
     def __init__(self, input_dim, hidden_dim=64, output_dim=5):
         super().__init__()
         
-        self.net == nn.Sequential(
+        self.net = nn.Sequential(
             nn.Linear(input_dim, hidden_dim),
             nn.ReLU(),
             nn.Linear(hidden_dim, output_dim)
