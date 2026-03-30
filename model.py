80dc1849d81142724537154c92c9d3f0a1ab5ccb
loganprosser <loganpros@gmail.com>
2026-03-30 14:08:11 -0400

    added torch

--- model.py
+++ model.py
@@ -2,7 +2,6 @@ import torch
 import numpy as np
 import matplotlib
 import pandas as pd
-from sklearn.model_selection import train_test_split
 
 
 # ====== import data from the csv =======
@@ -40,12 +39,21 @@ Y = df[TARGET_COLS].to_numpy(dtype=np.float32)
 X[X == -999.0] = 0.0
 
 # ====== TRAIN / VAL SPLIT ======
-X_train, X_val, Y_train, Y_val = train_test_split(
-    X, Y,
-    test_size=0.2,
-    random_state=42,
-    shuffle=True
-)
+n = len(X)
+val_fraction = 0.2
+n_val = int(n * val_fraction)
+
+# shuffle indices
+rng = np.random.default_rng(seed=42)
+indices = rng.permutation(n)
+
+val_idx = indices[:n_val]
+train_idx = indices[n_val:]
+
+X_train = X[train_idx]
+X_val   = X[val_idx]
+Y_train = Y[train_idx]
+Y_val   = Y[val_idx]
 
 # ====== CONVERT TO TENSORS ======
 X_train = torch.tensor(X_train, dtype=torch.float32)
